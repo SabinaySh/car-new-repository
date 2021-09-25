46f1a8b0fa820d6fb6b9ea3072da612bddebbfa8
SabinaySh <sabinaysh@code.edu.az>
2021-09-25 13:24:38 -0700

    added js code to showroom page

--- Car Final Project/assets/js/style.js
+++ Car Final Project/assets/js/style.js
@@ -3,10 +3,23 @@ $(document).ready(function(){
         items:1,
         loop:true,
     });
-    // $(".testimonial-owl").owlCarousel({
-    //     items:1,
-    //     loop:true,
-    // });
 
     $('.popup-btn').magnificPopup({type:'iframe'});
   });
+
+  let specification=document.querySelector(".specification-header");
+  let specifiactionList=document.querySelector(".specifiaction-list");
+
+  specification.addEventListener("click",function(){
+    specifiactionList.classList.toggle("active");
+  });
+
+  let feature=document.querySelector(".feature-header");
+  let featureList=document.querySelector(".feature-list");
+
+  feature.addEventListener("click",function(){
+    featureList.classList.toggle("active");
+  });
+
+
+  
