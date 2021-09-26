8ae27403672777241c849e23a3615d44729d23c8
SabinaySh <sabinaysh@code.edu.az>
2021-09-26 03:29:37 -0700

    addded js to loan calculate

--- Car Final Project/assets/js/style.js
+++ Car Final Project/assets/js/style.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
-    $(".img-testimonial").owlCarousel({
-        items:1,
-        loop:true,
-    });
+  // Start Carousel
+    // $(".img-testimonial").owlCarousel({
+    //     items:1,
+    //     loop:true,
+    // });
+    // // End Carousel
 
+    //Start Popup-Youtube
     $('.popup-btn').magnificPopup({type:'iframe'});
+    // End Popup-Youtube
   });
 
+  // Start Showroom page Specification Section
   let specification=document.querySelector(".specification-header");
   let specifiactionList=document.querySelector(".specifiaction-list");
 
@@ -20,6 +25,32 @@ $(document).ready(function(){
   feature.addEventListener("click",function(){
     featureList.classList.toggle("active");
   });
+// End Showroom page Specification Section
+
+// Start Credit page Loan Calculator Section
+let creditForm=document.querySelector("#credit-form");
+let price=document.querySelector('input[name="price"]');
+let rate=document.querySelector('input[name="rate"]');
+let month=document.querySelector('input[name="month"]');
+let down=document.querySelector('input[name="down-payment"]');
+let formControl=document.querySelectorAll('.form-control');
+
+  creditForm.addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    let priceValue=parseFloat(price.value);
+    let rateValue=parseFloat(rate.value);
+    let monthValue=parseFloat(month.value);
+    console.log(priceValue);
+
+    let total=priceValue + (rateValue * monthValue * priceValue)/100;
+    let downPayment=total - priceValue;
+
+    down.value=downPayment;
+  })
+// End Credit page Loan Calculator Section
+
 
 
   
